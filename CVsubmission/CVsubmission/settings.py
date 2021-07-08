b1ebf4d85f18f51af593bacac9277369a2b94523
raghadm7md <raghad.m7md.1417@gmail.com>
2021-07-08 11:51:23 +0300

    setup deploy to heroku

--- CVsubmission/CVsubmission/settings.py
+++ CVsubmission/CVsubmission/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 from dotenv import load_dotenv
 
 import os
+import django_heroku
+
 
 load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -145,6 +147,7 @@ CORS_ALLOW_METHODS = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
